6e0fd8886dea66c91a016d2b6b9e9322f5be00b9
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-10-02 16:55:16 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,6 +37,7 @@ def monopoly_with_third_tier
 	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
 	monopoly[:railroads][:names][:b_and_o_railroad] = {}
 	monopoly[:railroads][:names][:shortline] = {}
+	monopoly
 end
 
 def monopoly_with_fourth_tier
